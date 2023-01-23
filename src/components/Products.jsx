34b2580e9e70e9bbe4c4e17fdbddf2099ee8b8ac
petercoco626 <petercoco626@gmail.com>
2023-01-24 00:35:23 +0900

    [feat] useEffect return callback func 실행 시점

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,6 +13,9 @@ export default function Products() {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
         setProducts(data);
       });
+
+    /** return callback function은 해당 component가 unmount가 될때 실행된다. */
+    /** dependency에 명시된 값에 변화가 생기면 return함수가 발생한다. 값에 변확가 생겼다는건 해당 컴포넌트가 재랜더링이 됨으로 unmount -> mount되기 때문 */
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
