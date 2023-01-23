3fa32f6557eba7fa385fc022c58feb6e8b58316f
petercoco626 <petercoco626@gmail.com>
2023-01-24 00:29:28 +0900

    [feat] useEffet에 대해...

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Profile from './components/Profile';
 import Avatar from './components/Avatar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Counter from './components/Counter';
+import Products from './components/Products';
 
 function App() {
-  const avatars = [
-    {
-      image:
-        'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
-      name: 'name 1',
-      title: 'frontend 1',
-      isNew: true,
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80',
-      name: 'name 2',
-      title: 'frontend 2',
-      isNew: true,
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
-      name: 'name 3',
-      title: 'frontend 3',
-      isNew: false,
-    },
-  ];
+  const [avatars, setAvatars] = useState([]);
 
   const [totalCount, setTotalCount] = useState(0);
 
@@ -37,12 +16,24 @@ function App() {
     console.log(event);
   };
 
+  useEffect(() => {
+    fetch('data/avatars.json')
+      .then((res) => res.json())
+      .then((res) => {
+        setAvatars(res);
+        console.log('get avatar datas');
+      });
+    return () => {
+      console.log('clean avatar datas');
+    };
+  }, []);
+
   return (
     <>
       {/** 함수의 원형을 전달해야지 함수의 실행 결과를 전달하면 안된다 handleClick() : X  */}
       {/* <button onClick={handleClick}>버튼</button>
-      <Avatar image={avatars[0].image} isNew={avatars[0].isNew} />
-      {avatars.map((avatar) => (
+      <Avatar image={avatars[0]?.image} isNew={avatars[0]?.isNew} />
+      {avatars?.map((avatar) => (
         <Profile
           key={avatar.image}
           image={avatar.image}
@@ -51,13 +42,14 @@ function App() {
           isNew={avatar.isNew}
         />
       ))} */}
-      <div className="total-count">
+      {/* <div className="total-count">
         {totalCount > 10 ? `Total Count : ${totalCount} !!!!` : `Total Count : ${totalCount}`}
       </div>
       <div className="couter-wrap">
         <Counter totalCount={totalCount} handleClickCounter={handleClickCounter} />
         <Counter totalCount={totalCount} handleClickCounter={handleClickCounter} />
-      </div>
+      </div> */}
+      <Products />
     </>
   );
 }
